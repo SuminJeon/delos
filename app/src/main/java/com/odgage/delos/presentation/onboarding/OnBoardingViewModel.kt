7b489ee0a8b10f69cca43ecf637b2b55ef59a108
MyungjunChae <cmj9597@naver.com>
2020-01-08 20:25:41 +0900

    Add viewmodel

---
+++ app/src/main/java/com/odgage/delos/presentation/onboarding/OnBoardingViewModel.kt
@@ -0,0 +1,7 @@
+package com.odgage.delos.presentation.onboarding
+
+import androidx.lifecycle.ViewModel
+
+class OnBoardingViewModel() : ViewModel(){
+
+}
---
+++ app/src/main/java/com/odgage/delos/presentation/splash/SplashViewModel.kt
@@ -0,0 +1,7 @@
+package com.odgage.delos.presentation.splash
+
+import androidx.lifecycle.ViewModel
+
+class SplashViewModel() : ViewModel(){
+
+}
